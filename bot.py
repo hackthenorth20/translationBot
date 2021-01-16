c19dd69ae2a331de553043b75d93198e0ab2e280
fofsfofs <farhanfaruqui13@gmail.com>
2021-01-16 18:54:28 -0500

    smol qol stuf

--- bot.py
+++ bot.py
@@ -10,6 +10,13 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="$", intents=intents)
 
 
+@bot.event
+async def on_ready():
+    print(f"{bot.user} has connected to Discord!")
+    activity = discord.Game("Translating languages")
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+
+
 # Commands
 @bot.command()
 @commands.has_guild_permissions(administrator=True)
