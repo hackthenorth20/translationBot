3a1fd329a5377f70520797e0b7cbcf1a66969f33
fofsfofs <farhanfaruqui13@gmail.com>
2021-01-17 00:46:07 -0500

    Project almost complete Implemented role reacts for languages Microphone reacts to determine who speaks

--- cogs/ahmad.py
+++ cogs/ahmad.py
@@ -16,15 +16,13 @@ from pydub import AudioSegment
 
 discord.opus.load_opus(ctypes.util.find_library("opus"))
 discord.opus.is_loaded()
-
 user_audioMap = {}
 
-
-async def speechToText(ctx, file_name, j, vc):
+async def speechToText(file_name, j, vc, author, listenerLang, logging):
     global user_audioMap
-    if os.stat(file_name).st_size == 0 and len(user_audioMap[str(ctx.author.id)]) != 0:
-        local_map = user_audioMap[str(ctx.author.id)]
-        user_audioMap[str(ctx.author.id)] = []
+    if os.stat(file_name).st_size == 0 and len(user_audioMap[str(author.id)]) != 0:
+        local_map = user_audioMap[str(author.id)]
+        user_audioMap[str(author.id)] = []
         recognizer = speech_recognition.Recognizer()
         audios = []
 
@@ -34,27 +32,27 @@ async def speechToText(ctx, file_name, j, vc):
 
         combined = sum(audios)
         file_handle = combined.export(
-            f"combined_audio/{ctx.author.id}_{j}.wav", format="wav"
+            f"combined_audio/{author.id}_{j}.wav", format="wav"
         )
 
-        fp = open(f"combined_audio/{ctx.author.id}_{j}.wav", "rb")
+        fp = open(f"combined_audio/{author.id}_{j}.wav", "rb")
         with speech_recognition.AudioFile(fp) as source:
             sr_audio_data = recognizer.record(source)
 
-        text = recognizer.recognize_google(sr_audio_data, language="en-US")
-        await ctx.send(text)
-        # await speech.transcribe_streaming(ctx,f"combined_audio/{ctx.author.id}_{j}.wav")
-        translatedText = translateText(text, "en")
-        await ctx.send(translatedText)
-        tts("en-US", translatedText)
+        text = recognizer.recognize_google(sr_audio_data, language=author.roles[1].name)
+        await logging.send(text)
+        translatedText = translateText(text, listenerLang[:2])
+        await logging.send(translatedText)
+        tts(listenerLang, translatedText)
         audio_source = discord.FFmpegPCMAudio("output.mp3")
 
-        if not vc.is_playing():
-            vc.play(audio_source, after=None)
+        while vc.is_playing():
+            pass
+
+        vc.play(audio_source, after=None)
 
     elif os.stat(file_name).st_size != 0:
-        user_audioMap[str(ctx.author.id)].append(file_name)
-
+        user_audioMap[str(author.id)].append(file_name)
 
 class Ahmad(commands.Cog):
     def __init__(self, bot):
@@ -64,40 +62,72 @@ class Ahmad(commands.Cog):
     @commands.guild_only()
     async def join(self, ctx):
         voiceChannel = ctx.author.voice.channel
-        vc = await voiceChannel.connect()
         (Path.cwd() / "waves").mkdir(exist_ok=True)
         (Path.cwd() / "combined_audio").mkdir(exist_ok=True)
-        i = 0
-        global user_audioMap
-        user_audioMap[str(ctx.author.id)] = []
-        while True:
-            (Path.cwd() / f"waves/wave{i}.wav").touch(exist_ok=True)
-            fp = (Path.cwd() / f"waves/wave{i}.wav").open("rb")
-            vc.listen(
-                discord.UserFilter(discord.WaveSink(f"waves/wave{i}.wav"), ctx.author)
-            )
-            await asyncio.sleep(2)
-            vc.stop_listening()
-            asyncio.get_event_loop().create_task(
-                speechToText(ctx, f"waves/wave{i}.wav", i, vc)
-            )
-            i += 1
+        if len(voiceChannel.members) != 2:
+            await ctx.send("Only two members in a VC is allowed")
+        else:
+            msg = await ctx.send("React to the microphone to speak")
+            await msg.add_reaction("🎙️")
 
-    @commands.command()
-    @commands.guild_only()
-    async def test(self, ctx):
-        voiceChannel = ctx.author.voice.channel
-        userTwo = (
-            voiceChannel.members[0]
-            if voiceChannel.members[1] == ctx.author
-            else ctx.author
-        )
-        await ctx.send(userTwo)
+
+    # @commands.command()
+    # @commands.guild_only()
+    # async def test(self, ctx):
+    #     voiceChannel = ctx.author.voice.channel
+    #     userTwo = (
+    #         voiceChannel.members[0]
+    #         if voiceChannel.members[1] == ctx.author
+    #         else ctx.author
+    #     )
+    #     await ctx.send(userTwo)
 
     @commands.command()
     @commands.guild_only()
     async def leave(self, ctx):
         await ctx.voice_client.disconnect()
+
+
+    @commands.Cog.listener()
+    @commands.guild_only()
+    async def on_raw_reaction_add(self, payload):
+        guild = discord.utils.find(lambda g: g.id == payload.guild_id, self.bot.guilds)
+        channel = guild.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+
+        if payload.emoji.name == "🎙️" and message.reactions[0].count == 2:
+            micHolder = guild.get_member(payload.user_id)
+            voiceChannel = micHolder.voice.channel
+            logging = guild.get_channel(800234160458956830)
+            listener = None
+            for member in  voiceChannel.members:
+                if member != micHolder:
+                    listener = member
+
+            vc = await voiceChannel.connect()
+            i = 0
+            global user_audioMap
+            user_audioMap[str(micHolder.id)] = []
+            while message.reactions[0].count != 1:
+                (Path.cwd() / f"waves/{micHolder.id}_{i}.wav").touch(exist_ok=True)
+                fp = (Path.cwd() / f"waves/{micHolder.id}_{i}.wav").open("rb")
+                vc.listen(
+                    discord.UserFilter(discord.WaveSink(f"waves/{micHolder.id}_{i}.wav"), micHolder)
+                )
+                await asyncio.sleep(2)
+                vc.stop_listening()
+                asyncio.get_event_loop().create_task(
+                    speechToText(f"waves/{micHolder.id}_{i}.wav", i, vc, micHolder, listener.roles[1].name, logging)
+                )
+                i += 1
+                message = await channel.fetch_message(payload.message_id)
+            await vc.disconnect()
+        elif message.reactions[0].count > 2 and channel.id != 800177711423160360:
+            member = guild.get_member(payload.user_id)
+            await message.remove_reaction(payload.emoji, member)
+        elif payload.emoji.name != "🎙️" and channel.id != 800177711423160360:
+            await message.clear_reaction(payload.emoji)
+
 
 
 def setup(bot):
--- cogs/tamim.py
+++ cogs/tamim.py
@@ -2,64 +2,55 @@ import discord
 import json
 from discord.ext import commands, tasks
 
-def read_json():
-    with open("languages.json", "r") as read_file:
-        try:
-            json_object = json.load(read_file)
-        except:
-            print("JSON read error")
-    return json_object
+botID = 799857466367541258
+guildID = 766020518620299265
 
 
-def write_json(json_object):
-    with open("languages.json", "w") as write_file:
-        try:
-            json.dump(json_object, write_file)
-        except:
-            print("JSON write error")
+def role_select(guild, emoji):
+
+    role = None
+    if emoji == "1️⃣":
+        role = discord.utils.get(guild.roles, name="en-US")
+    elif emoji == "2️⃣":
+        role = discord.utils.get(guild.roles, name="fr-CA")
+    elif emoji == "3️⃣":
+        role = discord.utils.get(guild.roles, name="es-ES")
+    print(role)
+    # elif emoji == "4️⃣":
+    #     role = discord.utils.get(guild.roles, name="4th Year")
+    return role
 
 
 class Tamim(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
     #### Add team-roles on Reaction ####
     @commands.Cog.listener()
     @commands.guild_only()
     async def on_raw_reaction_add(self, payload):
-        json_object = read_json()
 
-        if payload.channel.id != json_object['channel']:
-            return
+        # if payload.channel.id != json_object["channel"]:
+        #     return
 
         if payload.user_id == botID:
             return
 
-        guild = discord.utils.find(
-            lambda g: g.id == payload.guild.id, self.client.guilds)
+        guild = discord.utils.find(lambda g: g.id == guildID, self.bot.guilds)
 
-        member = discord.utils.find(
-            lambda m: m.id == payload.user_id, guild.members)
+        member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
         if member is None:
             print("Member not found.")
             return
 
         roleName = None
-
-        for i in json_object['languages'].keys():
-            if json_object['languages'][i]["emojiName"] == payload.emoji.name:
-                roleName = json_object['languages'][i]["roleName"]
-                break
-
-        if roleName is None:
-            return
-
-        role = discord.utils.get(guild.roles, name=roleName)
+        print(payload.emoji.name)
+        role = role_select(guild, payload.emoji.name)
 
         # Create Embed
         confirm_embed = discord.Embed(
-            title=f"Roles assigned for {member}", color=0x2af761)
+            title=f"Roles assigned for {member}", color=0x2AF761
+        )
         confirm_embed.add_field(name="Member ID:", value=f"{member.id}")
         confirm_embed.add_field(name="Added:", value=f"{role}", inline=False)
         confirm_embed.set_thumbnail(url=f"{member.avatar_url}")
@@ -74,39 +65,24 @@ class Tamim(commands.Cog):
     @commands.Cog.listener()
     @commands.guild_only()
     async def on_raw_reaction_remove(self, payload):
-        json_object = read_json()
-
-        if payload.channel.id != json_object['channel']:
-            return
 
         # Excludes bot from role requests
         if payload.user_id == botID:
             return
 
-        guild = discord.utils.find(
-            lambda g: g.id == guildID, self.client.guilds)
+        guild = discord.utils.find(lambda g: g.id == guildID, self.bot.guilds)
 
-        member = discord.utils.find(
-            lambda m: m.id == payload.user_id, guild.members)
+        member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
         if member is None:
             print("Member not found.")
             return
 
-        roleName = None
-
-        for i in json_object['languages'].keys():
-            if json_object['languages'][i]["emojiID"] == payload.emoji.name:
-                roleName = json_object['languages'][i]["roleName"]
-                break
-
-        if roleName is None:
-            return
-
-        role = discord.utils.get(guild.roles, name=roleName)
+        role = role_select(guild, payload.emoji.name)
 
         # Create Embed
         confirm_embed = discord.Embed(
-            title=f"Roles removed for {member}", color=0x2af761)
+            title=f"Roles removed for {member}", color=0x2AF761
+        )
         confirm_embed.add_field(name="Member ID:", value=f"{member.id}")
         confirm_embed.add_field(name="Removed:", value=f"{role}", inline=False)
         confirm_embed.set_thumbnail(url=f"{member.avatar_url}")
@@ -123,36 +99,32 @@ class Tamim(commands.Cog):
     @commands.has_guild_permissions(administrator=True)
     async def lang_setup(self, ctx, message_content=None):
 
-        json_object = read_json()
-
-        json_object["channel"] = ctx.channel.id
-
-        guild = discord.utils.find(
-            lambda g: g.id == ctx.guild.id, self.client.guilds)
+        guild = discord.utils.find(lambda g: g.id == guildID, self.bot.guilds)
 
         message = None
         if message_content is None:
-            message = await ctx.send("Please provide the message content.")
+            message = await ctx.send("Choose a default language below.")
         else:
             message = await ctx.send(f"{message_content}:")
 
-        for lang in json_object['languages']:
-            emoji = discord.utils.get(
-                guild.emojis, name=json_object['languagess'][lang]["emojiName"])
-            await message.add_reaction(emoji)
-
-        json_object["message"] = message.id
-        json_object["messageContent"] = message.content
-
-        write_json(json_object)
+        langs = ["en-US", "fr-CA", "es-ES"]
+        emojis = ["1️⃣", "2️⃣", "3️⃣"]
 
         # Edit initial message
-        new_message_content = json_object['messageContent']
-        for lang in json_object['languages']:
-            emoji = discord.utils.get(
-                guild.emojis, name=json_object['languages'][lang]["emojiName"])
-            new_message_content += '\n' + \
-                f'{emoji} => {lang}'
+        new_message_content = "Choose a default language below."
+        for i in range(len(langs)):
+            await message.add_reaction(emojis[i])
+            new_message_content += "\n" + f"{emojis[i]} => {langs[i]}"
+            role = discord.utils.get(guild.roles, name=langs[i])
+            if role is None:
+                try:
+                    role = await guild.create_role(
+                        name=f"{langs[i]}", colour=discord.Colour(0xFFFFFF)
+                    )
+                except:
+                    await ctx.send("Failed to create Role.")
+                    return
+
         await message.edit(content=new_message_content)
 
 
